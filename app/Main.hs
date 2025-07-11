e5ee264c3dffea0875b9210197cbbdd88a570cc4
Christopher Weaver <crweaver225@yahoo.com>
2025-07-11 17:40:29 -0500

    added persistence

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,53 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
 import Web.Scotty
 import Control.Concurrent.MVar
-import qualified Data.Map as Map
-import qualified Data.ByteString.Lazy.Char8 as BL
-import qualified Data.Text.Lazy as TL
 
-type Store = MVar (Map.Map String Int)
+import qualified Data.Map as Map
+import Data.Map (Map)
+
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import qualified Data.ByteString.Lazy as BL
+
+import qualified Control.Exception as E
+import Control.Exception (IOException)
+
+import Data.Aeson (decode, encode)
+
+type Key = String
+type Store = Map Key Int
+
+storeFile :: FilePath
+storeFile = "store.json" 
 
 main :: IO ()
 main = do
-    -- Create a new MVar to store the key counts
-    store <- newMVar Map.empty
+    
+  storeData <- BL.readFile storeFile `E.catch` \(_ :: IOException) -> return "{}"
+  let initialStore = maybe Map.empty id (decode storeData)
+  store <- newMVar initialStore
 
-    scotty 9000 $ do
+  let saveStore = do
+        finalStore <- readMVar store
+        BL.writeFile storeFile (encode finalStore)
 
-        -- POST /input
-        post "/input" $ do
-            bodyData <- body -- Read the raw request body as a lazy ByteString
-            let key = BL.unpack bodyData  -- Convert ByteString to String
-            liftIO $ modifyMVar_ store $ \m ->
-                return $ Map.insertWith (+) key (1 :: Int) m
-            text "OK"
+  scotty 9000 $ do
+    post "/input" $ do
+      bodyText <- body
+      let key = T.unpack (TE.decodeUtf8 (BL.toStrict bodyText))
+      liftIO $ modifyMVar_ store $ \m ->
+        return $ Map.insertWith (+) key (1 :: Int) m
+      liftIO saveStore
+      text "OK"
 
-        -- GET /query?key=...
-        get "/query" $ do
-            key <- queryParam "key"
-            count <- liftIO $ do
-                m <- readMVar store
-                return $ Map.findWithDefault 0 key m
-            text (TL.pack (show count))
+    get "/query" $ do
+      key <- queryParam "key"
+      count <- liftIO $ do
+        m <- readMVar store
+        return $ Map.findWithDefault 0 key m
+      text (TL.pack (show count))
